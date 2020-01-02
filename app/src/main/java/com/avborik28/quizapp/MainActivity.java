0ac147c19ba77df7f8a169841a4fadfa91a355a7
AvBorikDeveloper <avboris28@gmail.com>
2020-01-03 03:36:31 +1300

    Added toast messages

--- app/src/main/java/com/avborik28/quizapp/MainActivity.java
+++ app/src/main/java/com/avborik28/quizapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +24,14 @@ public class MainActivity extends AppCompatActivity {
                 //Log.i("Listener ", "The button is tapped");
 
                 if(v.getId() == R.id.btnTrue){
-                    Log.i("Listener","Button true is tapped !!");
+                   // Log.i("Listener","Button true is tapped !!");
                 }
                 /*if(v.getId() == R.id.btnFalse){
                     Log.i("Listenter", "The false button is tapped");
+
                 }*/
+                Toast myToastObject = Toast.makeText(getApplicationContext(),"btn True is tapped now",Toast.LENGTH_LONG);
+                myToastObject.show();
             }
         };
 
@@ -40,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
               @Override
               public void onClick(View v) {
                   if(v.getId() == R.id.btnFalse){
-                      Log.i("another Listenter", "The false button is tapped");
+                      //Log.i("another Listenter", "The false button is tapped");
+                      /*Toast myToastObject = Toast.makeText(getApplicationContext(),"btn False is tapped now",Toast.LENGTH_LONG);
+                      myToastObject.show();*/
+                      Toast.makeText(getApplicationContext(),"btn Wrong is tapped now",Toast.LENGTH_LONG).show();
                   }
               }
           });
